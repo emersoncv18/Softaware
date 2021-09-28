2a0e2e25285df8d50c0db203bdfd59fb56a4b0a7
Emerson Cano <emerson.cano@correo.tdea.edu.co>
2021-09-27 21:35:58 -0500

    Implementación de la interfaz

--- src/main/java/net/cryls/backendcursojava/UserRepository.java
+++ src/main/java/net/cryls/backendcursojava/UserRepository.java
@@ -8,4 +8,8 @@ import net.cryls.backendcursojava.entities.UserEntity;
 @Repository
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
 
+
+    //public UserEntity findUserByFirstName(String name);
+    public UserEntity findUserByEmail(String email);
+    
 }
--- src/main/java/net/cryls/backendcursojava/services/UserService.java
+++ src/main/java/net/cryls/backendcursojava/services/UserService.java
@@ -1,17 +1,36 @@
 package net.cryls.backendcursojava.services;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import net.cryls.backendcursojava.UserRepository;
+import net.cryls.backendcursojava.entities.UserEntity;
 import net.cryls.backendcursojava.shared.dto.UserDTO;
 
 @Service
-public class UserService implements UserServiceInterface{
+public class UserService implements UserServiceInterface {
 
+    @Autowired
+    UserRepository userRepository;
 
     @Override
     public UserDTO createUser(UserDTO userDTO) {
+
+        UserEntity userEntity = new UserEntity();
+        BeanUtils.copyProperties(userDTO, userEntity);
+        userEntity.setEncryptedPassword("testpassword");
+        userEntity.setUserId("testUserId");
+
+        UserEntity storedUserDetail = userRepository.save(userEntity);
+
+        userRepository.save(userEntity);
+
+        UserDTO userToReturn = new UserDTO();
+        BeanUtils.copyProperties(userDTO, userToReturn);
+        return userToReturn;
+
         
-        return null;
     }
-    
+
 }
